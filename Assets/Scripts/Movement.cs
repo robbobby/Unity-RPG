0133504c2db3eb9afded229d74f055769863e1f7
Robert Hollingworth <robert@roberts-air.home>
2020-12-22 23:40:39 +0000

    Refacor Camera to be less expensive - git ignore is been shitty with the .idea/

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    private Ray _lastRay;
+    private Camera _camera;
+
+    private void Start() {
+        _camera = Camera.main;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            this._lastRay = _camera.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(this._lastRay.origin, this._lastRay.direction * 100 );
+        GetComponent<NavMeshAgent>().destination = target.position; 
+    }
+}

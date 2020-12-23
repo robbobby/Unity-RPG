06e0f8c9880e74efff082940d4d9b276c27f31a7
Robert Hollingworth <robert@roberts-air.home>
2020-12-23 10:18:01 +0000

    Added raycast movement

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Player {
+    public class Movement : MonoBehaviour {
+        [SerializeField] Transform target;
+        private Camera _camera;
+        private void Start() {
+            _camera = Camera.main;
+        }
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                MoveToCursor();
+            }
+        }
+        private void MoveToCursor() {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            bool hasHit = Physics.Raycast(ray, out var positionToMoveTo);
+            if (hasHit) {
+                GetComponent<NavMeshAgent>().destination = positionToMoveTo.point;
+            }
+        }
+    }
+}
